397169888de54b0c68585ef71a3099f524a5dc92
LLL <Zeikezan12345@gmail.com>
2026-02-24 07:43:17 +0700

    update

--- src/ThucHanh/Coffee.java
+++ src/ThucHanh/Coffee.java
@@ -10,22 +10,12 @@ public class Coffee extends Drink{
 
     @Override
     public double calculatePrice() {
-        if(hasMilk == true) {
-            price = price + 5000;
-        } else if(hasMilk == false) {
-            price = price;
-        }
-
-        return price;
+        return hasMilk ? price + 5000 : price;
     }
 
     @Override
     public void displayInfo() {
         super.displayInfo();
-        if(hasMilk == true) {
-            System.out.println("Có Sữa");
-        } else if(hasMilk == false) {
-            System.out.println("Đen đá");
-        }
+        System.out.println((hasMilk ? "Có sữa" : "Đen đá"));
     }
 }
